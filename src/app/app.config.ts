05bc3e30e5493d3048b741e2a2ba89d3626002f3
Grzegorz Iwon <grzesiekiwon20@hotmail.com>
2024-09-12 10:57:39 +0200

    - front-end for beauty salon using Angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,21 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptorsFromDi
+} from "@angular/common/http";
+import {HttpTokenInterceptor} from "./notopenaiservices/interceptor/http-token.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptorsFromDi()
+    ),
+
+    {provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptor, multi: true}
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LoginComponent} from "./pages/login/login.component";
+import {RegisterComponent} from "./pages/register/register.component";
+import {ActivateAccountComponent} from "./pages/activate-account/activate-account.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'activate-account', component: ActivateAccountComponent},
+  {
+    path: 'activities',
+    loadChildren: () => import('./modules/activity-routing.module').then(m => m.ActivityRoutingModule),
+  }
+];
