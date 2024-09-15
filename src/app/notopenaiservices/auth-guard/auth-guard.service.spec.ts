deb05983e17795c4f44f41fb3ef53618ee495421
Grzegorz Iwon <grzesiekiwon20@hotmail.com>
2024-09-15 09:30:39 +0200

    -

--- src/app/notopenaiservices/auth-guard/auth-guard.service.spec.ts
+++ src/app/notopenaiservices/auth-guard/auth-guard.service.spec.ts
@@ -1,16 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthGuardService } from './auth-guard.service';
+import { authGuard } from './auth-guard.service';
+import {CanActivateFn} from "@angular/router";
 
-describe('AuthGuardService', () => {
-  let service: AuthGuardService;
-
+describe('authGuard', () => {
+  const executeGuard: CanActivateFn = (...guardParameters) =>
+    TestBed.runInInjectionContext(() => authGuard(...guardParameters));
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthGuardService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(executeGuard).toBeTruthy();
   });
 });
